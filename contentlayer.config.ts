ad50d5fe37f854b10d58e37f1c3b77d799d5aea6
chanyDev <chanyd.dev@gmail.com>
2022-07-12 18:21:35 +0900

    feat : mdx 파일에 대한 스키마 정의

---
+++ contentlayer.config.ts
@@ -0,0 +1,34 @@
+import { defineDocumentType, makeSource } from 'contentlayer/source-files';
+// rehype plugin
+import rehypeSlug from 'rehype-slug';
+import rehypePrism from 'rehype-prism-plus';
+// remark plugin
+import remarkGfm from 'remark-gfm';
+
+export const Post = defineDocumentType(() => ({
+  name: 'Post',
+  filePathPattern: 'post/*.mdx',
+  contentType: 'mdx',
+  fields: {
+    title: { type: 'string', required: true },
+    publishedDate: { type: 'string', required: true },
+    description: { type: 'string', required: true },
+    thumbnail: { type: 'string' },
+    tags: { type: 'list', of: { type: 'string' } },
+  },
+  computedFields: {
+    slug: {
+      type: 'string',
+      resolve: (doc) => doc._raw.sourceFileName.replace(/\.mdx/, ''),
+    },
+  },
+}));
+
+export default makeSource({
+  contentDirPath: 'data',
+  documentTypes: [Post],
+  mdx: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [rehypeSlug, rehypePrism],
+  },
+});
