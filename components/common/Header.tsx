7371060dc1da2b266eb6a5dfa0291d23c8e55269
chanyDev <chanyd.dev@gmail.com>
2022-07-14 00:16:11 +0900

    style : 네비게이션 스타일 수정

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -3,16 +3,24 @@ import Link from 'next/link';
 const Header = () => {
   return (
     <div className="fixed top-0 left-0 w-full">
-      <header className="flex justify-between max-w-3xl py-8 mx-auto xl:max-w-5xl">
+      <header className="flex items-center justify-between max-w-3xl py-8 mx-auto xl:max-w-5xl">
         <div>
-          <h1 className="text-2xl font-bold">
+          <h1 className="text-2xl font-bold cursor-pointer">
             <Link href={'/'}>Logo</Link>
           </h1>
         </div>
-        <nav className="flex gap-8">
-          <Link href={'/'}>Post</Link>
-          <Link href={'/'}>Note</Link>
-          <Link href={'/'}>About</Link>
+        <nav>
+          <ul className="flex gap-8">
+            <li className="font-medium hover:text-teal-500">
+              <Link href={'/'}>Post</Link>
+            </li>
+            <li className="font-medium hover:text-teal-500">
+              <Link href={'/'}>Note</Link>
+            </li>
+            <li className="font-medium hover:text-teal-500">
+              <Link href={'/'}>About</Link>
+            </li>
+          </ul>
         </nav>
       </header>
     </div>
