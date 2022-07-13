e07ec03e9685d221e2b7a0fa9641432fb90cc901
chanyDev <chanyd.dev@gmail.com>
2022-07-13 23:47:49 +0900

    style : 컴포넌트 레이아웃 구현

---
+++ components/common/Footer.tsx
@@ -0,0 +1,23 @@
+import { EmailIcon, GithubIcon } from '@components/icons';
+
+const Footer = () => {
+  return (
+    <footer className="flex flex-col items-center gap-4 py-8">
+      <div className="flex gap-6">
+        <GithubIcon
+          href="https://github.com/chanyDev"
+          className="h-6 text-gray-400 fill-current"
+        />
+        <EmailIcon
+          href="chanyd.dev@gmail.com"
+          className="h-6 text-gray-400 fill-current"
+        />
+      </div>
+      <div className="text-sm text-gray-500 ">
+        Copyright © 2022 Dongchan Kim
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
