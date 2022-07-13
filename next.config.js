7fc4964e0405f6f71a1ba2a0e2d7c02ef5c635b0
chanyDev <chanyd.dev@gmail.com>
2022-07-13 23:46:00 +0900

    웹팩 svg 설정 추가

--- next.config.js
+++ next.config.js
@@ -3,4 +3,12 @@ const { withContentlayer } = require('next-contentlayer');
 module.exports = withContentlayer({
   reactStrictMode: true,
   swcMinify: true, // Experimental Features
+  // svg 설정 추가
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
 });
