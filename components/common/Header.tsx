7ea29c3d2de24f0b101aea1bbb8411c14e72ae37
chanyDev <chanyd.dev@gmail.com>
2022-07-14 22:43:42 +0900

    feat : about, post 라우터 추가

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -5,7 +5,7 @@ import ThemeSwitch from './ThemeSwitch';
 
 const Header = () => {
   return (
-    <div className="w-full z-100">
+    <div className="w-full">
       <header className="flex items-center justify-between max-w-3xl py-8 mx-auto xl:max-w-5xl">
         <div>
           <Link href={'/'}>
@@ -19,22 +19,17 @@ const Header = () => {
         <nav className="flex items-center text-lg">
           <ul className="hidden gap-4 md:flex">
             <li className="px-4 py-2 font-medium">
-              <Link href={'/'}>
+              <Link href={'/post'}>
                 <a className="underlined">Post</a>
               </Link>
             </li>
             <li className="px-4 py-2 font-medium">
-              <Link href={'/'}>
-                <a className="underlined">Note</a>
-              </Link>
-            </li>
-            <li className="px-4 py-2 font-medium">
-              <Link href={'/'}>
+              <Link href={'/about'}>
                 <a className="underlined">About</a>
               </Link>
             </li>
           </ul>
-          <div className="flex gap-4 ml-4">
+          <div className="flex gap-4 ml-8">
             <ThemeSwitch />
             <button className="p-3 transition border-2 border-gray-200 hover:border-black dark:border-gray-600 dark:hover:border-white md:hidden rounded-3xl">
               <MenuIcon className="w-5 h-5 fill-current" />
