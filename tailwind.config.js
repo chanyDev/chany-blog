adf86f465fcb4474ab80a348fb97d5ad7900f7a6
chanyDev <chanyd.dev@gmail.com>
2022-07-15 18:25:50 +0900

    style : fadeIn 커스텀 애니메이션 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        fadeIn: {
+          '0%': { display: 'none', opacity: '0' },
+          '100%': { display: 'block', opacity: '1' },
+        },
+      },
+      animation: {
+        fadeIn: 'fadeIn 0.3s ease-in-out',
+      },
+    },
   },
   plugins: [require('@tailwindcss/typography')],
 };
