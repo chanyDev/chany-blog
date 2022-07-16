d74730be712aafc300e452596c1957043aa86158
chanyDev <chanyd.dev@gmail.com>
2022-07-16 18:30:03 +0900

    feat : 게시물 상세 페이지 구현

--- pages/posts/[slug]/index.tsx
+++ pages/posts/[slug]/index.tsx
@@ -1,5 +1,37 @@
-const PostDetailPage = () => {
-  return <div>PostDetailPage</div>;
+import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from 'next';
+import { allPosts } from 'contentlayer/generated';
+
+import PostMDX from '@components/post/PostMDX';
+
+interface PageProps {
+  [key: string]: string | undefined;
+  slug: string;
+}
+
+const PostDetailPage = ({
+  post,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
+  return <PostMDX post={post} />;
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = allPosts.map((post) => ({ params: { slug: post.slug } }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { slug } = params as PageProps;
+  const post = allPosts.find((post) => post.slug === slug);
+
+  return {
+    props: {
+      post,
+    },
+  };
 };
 
 export default PostDetailPage;
