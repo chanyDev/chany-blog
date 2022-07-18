81ab0a62835f58979fd53e55ad7eaab6c45e7d71
chanyDev <chanyd.dev@gmail.com>
2022-07-19 01:24:57 +0900

    feat : 태그 목록 페이지 구현

--- pages/tags/index.tsx
+++ pages/tags/index.tsx
@@ -1,5 +1,34 @@
-const TagListPage = () => {
-  return <div>TagListPage</div>;
+import PostTag from '@components/post/PostTag';
+import { allPosts, Post } from 'contentlayer/generated';
+
+const TagListPage = ({ tags }: { tags: string[] }) => {
+  return (
+    <div className="divide-y divide-gray-300 dark:divide-gray-500">
+      <h2 className="py-6 text-4xl font-extrabold md:text-5xl">Tags</h2>
+      <div className="flex flex-wrap gap-1 py-6">
+        {tags.map((tag, i) => (
+          <PostTag tag={tag} key={`${tag}${i}`} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export const getStaticProps = async () => {
+  const tags = [
+    ...new Set(
+      allPosts.reduce<string[]>((prev, curr: Post) => {
+        curr.tags?.forEach((tag) => prev.push(tag));
+        return prev;
+      }, []),
+    ),
+  ];
+
+  return {
+    props: {
+      tags,
+    },
+  };
 };
 
 export default TagListPage;
