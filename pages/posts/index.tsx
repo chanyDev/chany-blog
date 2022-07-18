61e09562f2096b5cb0368ee5ce47ffb2ad2c8671
chanyDev <chanyd.dev@gmail.com>
2022-07-18 17:41:57 +0900

    feat : 모든 게시물 목록 구현

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,5 +1,51 @@
-const PostListPage = () => {
-  return <div>PostListPage</div>;
+import { allPosts, Post } from 'contentlayer/generated';
+
+import PostList from '@components/post/PostList';
+import { POST_PER_PAGE } from '@constants/index';
+
+interface PostListPageProps {
+  posts: Post[];
+  initialPosts: Post[];
+  pagenation: {
+    currentPage: number;
+    totalPages: number;
+  };
+}
+
+const PostListPage = ({
+  posts,
+  initialPosts,
+  pagenation,
+}: PostListPageProps) => {
+  return (
+    <PostList
+      posts={posts}
+      searchVisible={true}
+      initialPosts={initialPosts}
+      pagenation={pagenation}
+    />
+  );
+};
+
+export const getStaticProps = async () => {
+  const posts = allPosts.sort(
+    (a, b) =>
+      Number(new Date(b.publishedDate)) - Number(new Date(a.publishedDate)),
+  );
+  const initialPosts = posts.slice(0, POST_PER_PAGE);
+  const currentPage = 1;
+  const totalPages = Math.ceil(posts.length / POST_PER_PAGE);
+
+  return {
+    props: {
+      posts,
+      initialPosts,
+      pagenation: {
+        currentPage,
+        totalPages,
+      },
+    },
+  };
 };
 
 export default PostListPage;
