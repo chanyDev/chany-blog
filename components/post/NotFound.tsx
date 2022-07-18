86b551929280363b7844f3c97ebae8b0dba34fc8
chanyDev <chanyd.dev@gmail.com>
2022-07-18 16:12:11 +0900

    style : 레이아웃 구현

---
+++ components/post/NotFound.tsx
@@ -0,0 +1,9 @@
+const NotFound = () => {
+  return (
+    <div className="py-10 text-xl text-center text-gray-500 md:text-2xl dark:text-gray-400">
+      No Matching Posts Found
+    </div>
+  );
+};
+
+export default NotFound;
