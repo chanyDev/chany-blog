4da47ae59df8f86d77722e1aba6550a93a6ad4bf
chanyDev <chanyd.dev@gmail.com>
2022-07-20 16:06:04 +0900

    fix : 시스템 테마 적용 [#1]

--- components/common/ThemeSwitch.tsx
+++ components/common/ThemeSwitch.tsx
@@ -5,45 +5,44 @@ import { useEffect, useState } from 'react';
 
 const ThemeSwitch = () => {
   const [mounted, setMounted] = useState(false);
-  const { theme, setTheme } = useTheme();
+  const { theme, systemTheme, setTheme } = useTheme();
+  const currentTheme = theme === 'system' ? systemTheme : theme;
 
   const handleTheme = () => {
-    setTheme(theme === 'light' ? 'dark' : 'light');
+    setTheme(currentTheme === 'light' ? 'dark' : 'light');
   };
 
   useEffect(() => {
     setMounted(true);
   }, []);
 
-  return (
-    <>
-      {mounted && (
-        <button
-          aria-label="Theme Toggle Button"
-          type="button"
-          onClick={handleTheme}
-          className="p-3 transition border-2 border-gray-200 dark:hover:border-white hover:border-black rounded-3xl dark:border-gray-600"
-        >
-          <div className="relative w-5 h-5 overflow-hidden">
-            <span
-              className={`absolute inset-0 origin-[50%_100px] duration-700 ${
-                theme === 'light' ? 'rotate-0' : '-rotate-90'
-              }`}
-            >
-              <SunIcon className="text-black fill-current dark:text-white" />
-            </span>
+  if (!mounted) return null;
 
-            <span
-              className={`absolute inset-0 origin-[50%_100px] duration-700 ${
-                theme === 'dark' ? 'rotate-0' : 'rotate-90'
-              }`}
-            >
-              <MoonIcon className="text-black fill-current dark:text-white" />
-            </span>
-          </div>
-        </button>
-      )}
-    </>
+  return (
+    <button
+      aria-label="Theme Toggle Button"
+      type="button"
+      onClick={handleTheme}
+      className="p-3 transition border-2 border-gray-200 dark:hover:border-white hover:border-black rounded-3xl dark:border-gray-600"
+    >
+      <div className="relative w-5 h-5 overflow-hidden">
+        <span
+          className={`absolute inset-0 origin-[50%_100px] duration-700 ${
+            currentTheme === 'light' ? 'rotate-0' : '-rotate-90'
+          }`}
+        >
+          <SunIcon className="text-black fill-current dark:text-white" />
+        </span>
+
+        <span
+          className={`absolute inset-0 origin-[50%_100px] duration-700 ${
+            currentTheme === 'dark' ? 'rotate-0' : 'rotate-90'
+          }`}
+        >
+          <MoonIcon className="text-black fill-current dark:text-white" />
+        </span>
+      </div>
+    </button>
   );
 };
 
